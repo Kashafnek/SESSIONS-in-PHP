4bda72bbba6f818e8345d38ed0c60188c504c867
Kashafnek <kashafnek17@gmail.com>
2023-11-16 18:01:24 +0500

    SESSIONS IN PHP

---
+++ WELCOME SESSION.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+echo "THIS IS THE WELCOME PAGE ONCE YOU LOGGED IN THEN YOU CAN SEE YOUR ID!";
+if (isset($_SESSION["username"])) {
+
+    echo "<h1>welcome in our website ".$_SESSION["username"]."</h1>";
+    echo "<b>favourite hobby ".$_SESSION["favourite_hobby"]."</b>";
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Home</span>
+        <form action="SESSION LOG OUT.php" method="post">
+          <button type="submit" class="btn active" aria-pressed="true">LOG OUT</button>
+        </form>
+      </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
